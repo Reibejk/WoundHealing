6840b06c07bbed867ee32a0d5beaa1951b85612e
Reibejk <144685099+Reibejk@users.noreply.github.com>
2024-03-25 14:27:22 +0100

    Update WoundHealing.js

--- WoundHealing.js
+++ WoundHealing.js
@@ -125,16 +125,16 @@ let slideIndex = 0;
 let count = 0;
 
 let imageObjectArray = [
-	{src: "./WoundRepair/P19b/P19b_110124.jpg", rotate: "0deg", top: "19px", left: "12px"},  
-	{src: "./WoundRepair/P19b/P19b_180124.jpg", rotate: "18deg", top: "7px", left: "-14px"}, 
-	{src: "./WoundRepair/P19b/P19b_220124.jpg", rotate: "2deg", top: "14px", left: "5px"}, 
-	{src: "./WoundRepair/P19b/P19b_250124.jpg", rotate: "5deg", top: "55px", left: "31px"}, 
-	{src: "./WoundRepair/P19b/P19b_290124.jpg", rotate: "-3deg", top: "34px", left: "19px"}, 
-	{src: "./WoundRepair/P19b/P19b_050224.jpg", rotate: "13deg", top: "116px", left: "-23px"},
-	{src: "./WoundRepair/P19b/P19b_070224.jpg", rotate: "0deg", top: "66px", left: "7px"},
-	{src: "./WoundRepair/P19b/P19b_150224.jpg", rotate: "7deg", top: "101px", left: "18px"},
-	{src: "./WoundRepair/P19b/P19b_200224.jpg", rotate: "15deg", top: "94px", left: "-1px"},
-	{src: "./WoundRepair/P19b/P19b_260224.jpg", rotate: "-176deg", top: "-56px", left: "-1px"},
+	{src: "./WoundRepair/P19b/P19b_110124.jpg", rotate: "0deg", top: "19px", left: "12px", clip: "rect(0px, 100px, 0px, 100px)"},  
+	{src: "./WoundRepair/P19b/P19b_180124.jpg", rotate: "18deg", top: "7px", left: "-14px", clip: "rect(0px, 100px, 0px, 100px)"}, 
+	{src: "./WoundRepair/P19b/P19b_220124.jpg", rotate: "2deg", top: "14px", left: "5px", clip: "rect(0px, 100px, 0px, 100px)"}, 
+	{src: "./WoundRepair/P19b/P19b_250124.jpg", rotate: "5deg", top: "55px", left: "31px", clip: "rect(0px, 100px, 0px, 100px)"}, 
+	{src: "./WoundRepair/P19b/P19b_290124.jpg", rotate: "-3deg", top: "34px", left: "19px", clip: "rect(0px, 100px, 0px, 100px)"}, 
+	{src: "./WoundRepair/P19b/P19b_050224.jpg", rotate: "13deg", top: "116px", left: "-23px", clip: "rect(0px, 100px, 0px, 100px)"},
+	{src: "./WoundRepair/P19b/P19b_070224.jpg", rotate: "0deg", top: "66px", left: "7px", clip: "rect(0px, 100px, 0px, 100px)"},
+	{src: "./WoundRepair/P19b/P19b_150224.jpg", rotate: "7deg", top: "101px", left: "18px", clip: "rect(0px, 100px, 0px, 100px)"},
+	{src: "./WoundRepair/P19b/P19b_200224.jpg", rotate: "15deg", top: "94px", left: "-1px", clip: "rect(0px, 100px, 0px, 100px)"},
+	{src: "./WoundRepair/P19b/P19b_260224.jpg", rotate: "-176deg", top: "-56px", left: "-1px", clip: "rect(0px, 100px, 0px, 100px)"},
 	];
 
 let imageObjectArray2 = [
@@ -161,6 +161,7 @@ function imageSlide() {
 			imgSlide.style.rotate = imageObjectArray[slideIndex].rotate;
 			imgSlide.style.top = imageObjectArray[slideIndex].top;
 			imgSlide.style.left = imageObjectArray[slideIndex].left;
+			imgSlide.style.clip = imageObjectArray[slideIndex].clip;
 		});
 		document.querySelector("#nameS").innerHTML = ("Date: ").concat(imageObjectArray[slideIndex].src.substring(24, 30));
 	} else {
@@ -172,13 +173,28 @@ function imageSlide() {
 		slideIndex = 0;
 	}
 }
+/*
+let imgIsLarge = "false";
 
-/*function stopImageSlide() {
-	if (slideImage) {
+let id = document.querySelector("#id")
 
-	}
-}*/
+function enlargeImg() {
+	for(let i = 0)
+	document.querySelector(".pic") = pic;
+		if(imgIsLarge = "false") {
+			document.querySelector(".pic").style.width = "400px";
+			imgIsLarge = "true";
+		}
+		else {
+			document.querySelector(".pic").style.width = "100px";
+			imgIsLarge = "false";
+	} 
+}
+*/
 
+
+
+//document.querySelector(".pic").addEventListener("click", enlargeImg);
 document.querySelector("#scale").addEventListener("change", changeScale);
 opacity.addEventListener("change", changeOpacity);
 document.querySelector("#merge").addEventListener("click", mergeImage);
@@ -194,26 +210,3 @@ document.querySelector("#left").addEventListener("dblclick", leftD);
 document.querySelector("#right").addEventListener("click", right);
 document.querySelector("#right").addEventListener("dblclick", rightD);
 document.querySelector("#imageSlide").addEventListener("click", imageSlide);
-
-
-/*setInterval(imageSlide, 1000);
-
-
-
-const opaVal = document.getElementById("opacity");
-
-opaVal.addEventListener("change", function(e) {
-	const opacity = e.target.value;
-	overLay(opacity);
-})
-
-
-
-function overLay(opacity) {
-	const img = document.getElementById("image");
-	img.style.transform = 'opacity(' + opacity + ')';
-}
-
-
-
-image.addEventListener("click", overLay);*/
